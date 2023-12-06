fbd0521fc69e08315a5d80a47a6b37f365961ebc
yh9035926 <yh9035926@naver.com>
2023-12-06 09:43:55 +0900

    indexOf

--- new Map.js
+++ new Map.js
@@ -3,9 +3,9 @@ const myMap = new Map();
 myMap.set("key1", "value1");
 console.log(myMap.get("key1")); //key에 해당하는 value 값을 반환
 
-console.log(myMap.has("key1")); //존재하면 true
+console.log(myMap.has("key1")); //존재하면 true , value값은 has로 못 불러옴
 
-console.log(myMap.delete("key1")); // Outputs: true
+console.log(myMap.delete("key1")); //삭제할 게 없으면 false
 
 console.log(myMap.size); //위에서 삭제해서 아무것도 없으므로 0
 
