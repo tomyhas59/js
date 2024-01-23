c3fbf7e5c8aa0f6ede48570beb165f1969b00f8b
yh9035926 <yh9035926@naver.com>
2024-01-24 05:26:43 +0900

    forEach((a, i, arr)

--- forEachMapFilter.js
+++ forEachMapFilter.js
@@ -1,14 +1,11 @@
 //forEach for문처럼 사용하는 용도
-a = [10, 11, 12, 13, 14, 15];
+const a = [10, 11, 12, 13, 14, 15];
 
-a.forEach(
-  function (a, i) {
-    console.log(a * a, i, this /*[3, 4, 5] */);
-  },
-  [3, 4, 5]
-);
+a.forEach((a, i, arr) => {
+  console.log(a * a, i, arr);
+});
 
-//map 새로운 배열로 만듦
+//map 새로운 배열로 만듦, 값 반환됨
 let answerMap = a.map((a, i) => {
   return a * i;
 });
@@ -18,4 +15,4 @@ console.log(answerMap); //[ 0, 11, 24, 39, 56, 75 ]
 let answerFilter = a.filter((v, i) => {
   return v % 2 === 0;
 });
-console.log(answerFilter);
+console.log(answerFilter); //[ 10, 12, 14 ]
--- sliceSort.js
+++ sliceSort.js
@@ -1,13 +1,13 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const array = [1, 3, 2, 4, 7, 6, 5, 8];
 
-console.log(array.slice(3, 6)); //4, 5, 6  array 원본 그대로 유지
-console.log(array.slice(3, -3)); //4, 5 두 번째 인수인 -3까지 자름
+console.log(array.slice(3, 6)); //[ 4, 7, 6 ]  array 원본 그대로 유지
+console.log(array.slice(3, -3)); //[ 4, 7 ] 두 번째 인수인 -3까지 자름
 
 array.sort((a, b) => {
   return a - b; // 오름차순
 });
 //array 원본 배열 바꿈
-console.log(array);
+console.log(array); //[ 1, 2, 3, 4, 5, 6, 7, 8]
 
 array.slice().sort((a, b) => {
   return b - a; // 내림차순
