30d1907bade504f66f00dc86068d5b0f8b782594
yh9035926 <yh9035926@naver.com>
2024-03-04 09:18:03 +0900

    Array.from(num.toString(), Number);

--- Array.from.js
+++ Array.from.js
@@ -7,14 +7,18 @@ const str = "Hello";
 const charArray = Array.from(str); // ["H", "e", "l", "l", "o"]
 console.log(charArray.at(1)); //e
 
+const num = 12345;
+//정수를 배열로 변환
+const numArr = Array.from(num.toString(), Number); //[ 1, 2, 3, 4, 5 ]
+console.log(numArr);
+
 const originalArray = [1, 2, 3];
 const copyArray = Array.from(originalArray); // [1, 2, 3]
 
 const arrOf = Array.of(1, 2, 3); //[ 1, 2, 3 ]
-const arr = Array(1, 4, 5); //[ 3, 4, 5 ]
+const arr = Array(1, 4, 5); //[ 1, 4, 5 ]
 const length = arr.push(50); //arr의 길이 반환
 
 //[1, 4, 5, 50]
 console.log(arr.fill(3, 2)); //채울 값, ~부터 [ 1, 4, 3, 3 ]
 console.log(arr.fill(25, 1, 2)); //채울 값, ~부터, ~ 전까지 [ 1, 25, 3, 3 ]
-
--- substring.js
+++ substring.js
@@ -1,4 +1,4 @@
-const str = "beautiful";
+const str = "beautifulb";
 console.log(str.substring(3, 6));
 //3인덱스부터 6인덱스까지 문자 추출
 console.log(str.substring(3)); //3인덱스부터 끝까지 추출
